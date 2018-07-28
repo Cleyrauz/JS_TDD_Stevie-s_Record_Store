db0b739c0df88b5f584281bafc63bc8f27edc141
Cleyra Uzcategui <cleyra.uzcategui@gmail.com>
2018-07-28 16:56:01 +0100

    Added value of collection and tested

--- specs/customer_spec.js
+++ specs/customer_spec.js
@@ -4,10 +4,11 @@ const Record = require("../record");
 const Customer = require("../customer");
 
 describe("Customer", function(){
-  let customer1, record1, recordStore;
+  let customer1, customer2, record1, recordStore;
 
   beforeEach(function(){
     customer1 = new Customer("Cleyra", 20);
+    customer2 = new Customer("Jose", 100);
     record1 = new Record("Natalia Oreiro", "Me muero de amor", "latin pop", 10);
     record2 = new Record("Monsieur Periné", "Encanto tropical", "pop", 20);
     recordStore = new RecordStore("Stevie's Record Store", "Glasgow");
@@ -21,7 +22,7 @@ it('should have pocket money', function(){
   assert.strictEqual(20, customer1.pocket_money);
 })
 
-it("should be able to buy record from store", function(){
+it('should be able to buy record from store', function(){
   recordStore.addRecordToInventory(record1);
   recordStore.addRecordToInventory(record2);
   customer1.buyRecord(recordStore, record1);
@@ -31,5 +32,12 @@ it("should be able to buy record from store", function(){
   assert.deepStrictEqual(1, recordStore.inventory.length);
 })
 
+it('should be able to view the total value of their collection', function(){
+  recordStore.addRecordToInventory(record1);
+  recordStore.addRecordToInventory(record2);
+  customer2.buyRecord(recordStore, record1);
+  customer2.buyRecord(recordStore, record2);
+  assert.deepStrictEqual(30, customer2.valueOfCollection());
+})
 
 });
